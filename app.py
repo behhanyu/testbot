41e252cffb635ba43ee1f13b8ff5aa64a93a7930
hanyu46 <106042169+hanyu46@users.noreply.github.com>
2022-06-06 22:05:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,74 +48,24 @@ def callback():
 
     return 'OK'
 
-# 訊息傳遞區塊
+
+
+#訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text = event.message.text
-    if re.match('開始', message):
-        buttons_template_message = TemplateSendMessage(
-            alt_text='這個看不到',
-            template=ButtonsTemplate(
-                title='Menu',
-                text='請選擇類型',
-                actions=[
-                    PostbackTemplateAction(
-                        label='酒吧',
-                        text='酒吧'
-                        data='A&酒吧'
-                    ),
-                    PostbackTemplateAction(
-                        label='旅館',
-                        text='旅館'
-                        data='A&旅館'
-                    ),
-                    PostbackTemplateAction(
-                        label='全都要',
-                        text='全都要'
-                        data='A&全都要'
-                    ),
-                ]
-            )
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        # Flex Message Simulator網頁：https://developers.line.biz/console/fx/
+        flex_message = FlexSendMessage(
+            alt_text='行銷搬進大程式',
+            contents={...} #json貼在這裡
         )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
-    elif message == '酒吧' or message == '旅館' or message == '全都要':
-        bar_or_hotel = event.postback.data[2:]
-        flex_message = TextSendMessage(text='請輸入台北市的任意地區（區或街道），或點擊下列的快捷鍵選擇地區',  # （暫時只能做到有選項，無法自由填入）
-                                       quick_reply=QuickReply(items=[
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="中正區", text="中正區", data='B&' + bar_or_hotel + '&中正區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="萬華區", text="萬華區", data='B&' + bar_or_hotel + '&萬華區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="大同區", text="大同區", data='B&' + bar_or_hotel + '&大同區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="中山區", text="中山區", data='B&' + bar_or_hotel + '&中山區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="松山區", text="松山區", data='B&' + bar_or_hotel + '&松山區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="大安區", text="大安區", data='B&' + bar_or_hotel + '&大安區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="信義區", text="信義區", data='B&' + bar_or_hotel + '&信義區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="內湖區", text="內湖區", data='B&' + bar_or_hotel + '&內湖區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="南港區", text="南港區", data='B&' + bar_or_hotel + '&南港區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="士林區", text="士林區", data='B&' + bar_or_hotel + '&士林區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="北投區", text="北投區", data='B&' + bar_or_hotel + '&北投區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="文山區", text="文山區", data='B&' + bar_or_hotel + '&文山區')),
-                                       ]))
         line_bot_api.reply_message(event.reply_token, flex_message)
     else:
-        result = event.postback.data[2:].split('&')
-
-
-# 主程式
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#主程式
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
