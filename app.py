1a88c1f676f09956f6103dcce5a2388e268a414c
hanyu46 <106042169+hanyu46@users.noreply.github.com>
2022-06-07 08:57:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,17 +60,17 @@ def handle_message(event):
                     PostbackTemplateAction(
                         label='酒吧',
                         display_text='酒吧',
-                        data='A 酒吧'
+                        data='A酒吧'
                     ),
                     PostbackTemplateAction(
                         label='旅館',
                         display_text='旅館',
-                        data='A 旅館'
+                        data='A旅館'
                     ),
                     PostbackTemplateAction(
                         label='全都要',
                         display_text='全都要',
-                        data='A 全都要'
+                        data='A全都要'
                     )
                 ]
             )
@@ -79,35 +79,20 @@ def handle_message(event):
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
-    if(event.postback.data.startswith('A')):
-        flex_message = TextSendMessage(text='請輸入台北市的任意地區（區或街道），或點擊下列的快捷鍵選擇地區',  # （暫時只能做到有選項，無法自由填入）
-                                       quick_reply=QuickReply(items=[
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="中正區", text="中正區", data='B&' + bar_or_hotel + '&中正區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="萬華區", text="萬華區", data='B&' + bar_or_hotel + '&萬華區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="大同區", text="大同區", data='B&' + bar_or_hotel + '&大同區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="中山區", text="中山區", data='B&' + bar_or_hotel + '&中山區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="松山區", text="松山區", data='B&' + bar_or_hotel + '&松山區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="大安區", text="大安區", data='B&' + bar_or_hotel + '&大安區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="信義區", text="信義區", data='B&' + bar_or_hotel + '&信義區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="內湖區", text="內湖區", data='B&' + bar_or_hotel + '&內湖區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="南港區", text="南港區", data='B&' + bar_or_hotel + '&南港區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="士林區", text="士林區", data='B&' + bar_or_hotel + '&士林區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="北投區", text="北投區", data='B&' + bar_or_hotel + '&北投區')),
-                                            QuickReplyButton(action=PostbackAction(
-                                                label="文山區", text="文山區", data='B&' + bar_or_hotel + '&文山區')),
-                                       ]))
-        line_bot_api.reply_message(event.reply_token, flex_message)        
+    if event.postback.data[0:1] == "A":
+        flex_message = TextSendMessage(text='以下有雷，請小心',
+                               quick_reply=QuickReply(items=[
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="別按我", text="你按屁喔！爆炸了拉！！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+                                   QuickReplyButton(action=MessageAction(label="按我", text="按！"))
+                               ]))
+        line_bot_api.reply_message(event.reply_token, flex_message)  
         
 #主程式
 import os
