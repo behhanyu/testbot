4047254d369420d585130094d384e3f6977719f1
hanyu46 <106042169+hanyu46@users.noreply.github.com>
2022-06-08 10:25:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -96,6 +96,9 @@ def handle_message(event):
             )
         )
             line_bot_api.reply_message(event.reply_token, buttons_template_message)
+        else:
+            message = TextSendMessage(text = location())
+            line_bot_api.reply_message(event.reply_token, message)		            
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
