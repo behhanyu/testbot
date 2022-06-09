e65f4bfc8996204747711fdd523661ebcfd1aac3
hanyu46 <106042169+hanyu46@users.noreply.github.com>
2022-06-09 10:00:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -129,6 +129,9 @@ def handle_postback(event):
     elif event.postback.data[0:1] == "B":
         place_type = event.postback.data[4:]
         result = event.postback.data[2:].split('&')
+        crawl = location(result[1])
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=crawl))
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage('請輸入捷運站名'))
 
